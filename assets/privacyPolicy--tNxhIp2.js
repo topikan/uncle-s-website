48ecb256c2e735640445fd00f39b4c392efcec9a
tapochkin <topikofff@gmail.com>
2026-07-30 14:56:58 +0300

    Updates

---
+++ assets/privacyPolicy--tNxhIp2.js
@@ -0,0 +1,1 @@
+import{t as e}from"./popup-BJ5RQFui.js";fetch(`./popup-booking.html`).then(e=>e.text()).then(t=>{document.body.insertAdjacentHTML(`beforeend`,t),e()});
--- assets/rules-DVDEJCQg.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./popup-dI03T9i1.js";fetch(`./popup-booking.html`).then(e=>e.text()).then(t=>{document.body.insertAdjacentHTML(`beforeend`,t),e()});
